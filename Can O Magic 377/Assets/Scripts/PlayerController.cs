2bac2461193ff5235fa56afc0f27a41c24edeb53
BryanARamirez <103203508+BryanARamirez@users.noreply.github.com>
2024-02-13 15:12:41 -0800

    Player Controller Completed with Random Magic Item

--- Can O Magic 377/Assets/Scripts/PlayerController.cs
+++ Can O Magic 377/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,28 +10,62 @@ public class PlayerController : MonoBehaviour
     private int rightDis = 5;
     //How far the touch for dragging goes to the left
     private int leftDis = -5;
-    //Locks the height while dragging object
-    private int heightDis = 1000;
+    [SerializeField] private GameObject currentObj;
+    [SerializeField] private int nextObjIndex;
+    [SerializeField] private int currentObjIndex;
+    [SerializeField] private List<GameObject> magicObj = new List<GameObject>();
+    [SerializeField] private SteamScript steamScript;
+    private int randomNextIndex;
+    private bool isWaiting;
 
+    private void Awake()
+    {
+        isWaiting = false;
+        int randomIndex = Random.Range(0, magicObj.Count);
+        currentObjIndex = randomIndex;
+        currentObj = Instantiate(magicObj[randomIndex]);
+        currentObj.transform.parent = this.transform;
+        currentObj.transform.position = this.transform.position;
+        currentObj.GetComponent<Rigidbody>().useGravity = false;
+        randomNextIndex = Random.Range(0, magicObj.Count);
+        nextObjIndex = randomNextIndex;
+    }
 
     void Update()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+            Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, 0, 10));
             if (touchedPos.x <= rightDis && touchedPos.x >= leftDis)
             {
                 if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
                 {
-                    Vector3 lockedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, heightDis, 10));
+                    Vector3 lockedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, 0, 10));
+                    lockedPos.y = this.transform.position.y;
                     transform.position = lockedPos;
                 }
-                if (touch.phase == TouchPhase.Ended && touchedPos.x <= rightDis && touchedPos.x >= leftDis)
+                if (touch.phase == TouchPhase.Ended && touchedPos.x <= rightDis && touchedPos.x >= leftDis && isWaiting == false)
                 {
-                    //Place something that triggers the object to fall here.
+                    isWaiting = true;
+                    currentObj.GetComponent<Rigidbody>().useGravity = true;
+                    currentObj.transform.parent = null;
+                    StartCoroutine(spawnNext(1));
+                    steamScript.OnDrop();
                 }
             }
         }
+    }
+
+    IEnumerator spawnNext(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        isWaiting = false;
+        currentObj = Instantiate(magicObj[randomNextIndex]);
+        currentObj.transform.parent = this.transform;
+        currentObj.transform.position = this.transform.position;
+        currentObjIndex = randomNextIndex;
+        randomNextIndex = Random.Range(0, magicObj.Count);
+        currentObj.GetComponent<Rigidbody>().useGravity = false;
     }
 }
