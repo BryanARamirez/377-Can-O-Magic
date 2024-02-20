c27af50d7d3419be3eaa2a270dff202e1a5e85c6
Justin Lam <jlam123321@gmail.com>
2024-02-19 17:53:53 -0800

    reaction script to give power items

---
+++ Can O Magic 377/Assets/Scripts/MagicalItem/ReactionScripts/GetPowerItemScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [02/19/2024]
+ * [gives power item when magic item reacts]
+ */
+
+public class GetPowerItemScript : BaseReactionScript
+{
+    //what power item is given when reacted
+    [SerializeField] private PowerItemEnum _getPowerItem;
+
+    /// <summary>
+    /// tells the PowerItemData class which magic item to give
+    /// </summary>
+    /// <param name="otherItem"></param>
+    public override void Reaction(GameObject otherItem)
+    {
+        PowerItemData.Instance.GainPowerItem(_getPowerItem);
+    }
+}
