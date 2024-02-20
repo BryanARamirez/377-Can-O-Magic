e34c16c4d09140494ab86f644688ea84304d5809
Justin Lam <jlam123321@gmail.com>
2024-02-19 19:43:59 -0800

    made tsunami

--- Can O Magic 377/Assets/Scripts/MagicalItem/ReactionScripts/WaterBlueReaction.cs
+++ Can O Magic 377/Assets/Scripts/MagicalItem/ReactionScripts/WaterBlueReaction.cs
@@ -10,13 +10,45 @@ using UnityEngine;
 
 public class WaterBlueReaction : BaseReactionScript
 {
+    //game object for steam
+    private GameObject _steam;
+    [SerializeField] private Transform _topRightOfCan;
+    [SerializeField] private Transform _bottomLeftOfCan;
+
     private void Reset()
     {
         _reactionFor = MagicItemEnum.BlueRune;
     }
 
+    /// <summary>
+    /// get steam game object
+    /// </summary>
+    private void Awake()
+    {
+        _steam = GameObject.FindWithTag("Steam");
+        _topRightOfCan = GameObject.FindGameObjectWithTag("TopRight").transform;
+        _bottomLeftOfCan = GameObject.FindGameObjectWithTag("BottomLeft").transform;
+    }
+
     public override void Reaction(GameObject otherItem)
     {
-        Debug.Log("tsunami");
+        GameObject[] AllPowerItems = GameObject.FindGameObjectsWithTag("PowerItem");
+
+        foreach (GameObject powerItem in AllPowerItems)
+        {
+            Destroy(powerItem);
+        }
+
+        GameObject[] AllMagicItems = GameObject.FindGameObjectsWithTag("MagicItem");
+        foreach (GameObject magicItem in AllMagicItems)
+        {
+            if (magicItem.GetComponent<MagicalItemScript>().magicItemName == MagicItemEnum.FireOrb)
+            {
+                _steam.GetComponent<SteamScript>().ActivateSteam();
+                magicItem.GetComponent<MagicalItemScript>().Reacted();
+            } 
+
+            magicItem.transform.position = new Vector3(Random.Range(_bottomLeftOfCan.position.x, _topRightOfCan.position.x), Random.Range(_bottomLeftOfCan.position.y, _topRightOfCan.position.y), 0f);
+        }
     }
 }
