8b92f69ab489fbd68efc24075bdf1263c1dc3e1c
BryanARamirez <103203508+BryanARamirez@users.noreply.github.com>
2024-04-07 12:58:23 -0700

    Some sound changes

--- Can O Magic 377/Assets/Scripts/GameData.cs
+++ Can O Magic 377/Assets/Scripts/GameData.cs
@@ -56,7 +56,7 @@ public class GameData : Singleton<GameData>
         
         DontDestroyOnLoad(this.gameObject);
         //For some reason Load(); needs to be active when testing on PC but not when on the actual phone.
-        //Load();
+        Load();
         RankScores();
     }
     private void OnApplicationQuit()
@@ -195,7 +195,7 @@ public class GameData : Singleton<GameData>
                 new GameDataContainer{highScore = gameData.fourthScore, playerName = gameData.fourthName},
                 new GameDataContainer{highScore = gameData.fifthScore, playerName = gameData.fifthName}
             };
-            soundSettings.SetVolume(gameData.musicVolume);
+            soundSettings.SetMusicVolume();
         }
 
         if(File.Exists(Application.persistentDataPath + "/sceneData.dat"))
